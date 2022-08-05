38b767e4d580032c28513ba6b6ee00aecb698e0f
kovus380 <kosaf888@gmail.com>
2022-08-05 18:15:39 +0900

    Start Activity & Login Activity xml

---
+++ app/src/main/java/com/example/mbtm/FirstActivity.kt
@@ -0,0 +1,21 @@
+package com.example.mbtm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mbtm.databinding.ActivityFirstBinding
+
+class FirstActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityFirstBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.firstLoginBtn.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+    }
+}
